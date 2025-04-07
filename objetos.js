19cf030881ff9499fad32ab71382634e901a8d3c
Gustavo869 <162450568+Gustavo869@users.noreply.github.com>
2025-04-07 08:47:29 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,62 @@
+//Objetos//
+let times=[
+   {
+      nome: "Marreco Futsal",
+      goleiro: "Rafa Freitas",
+      aladireita: "Meleca",
+      alaesquerda: "Murilo",
+      pivô: "Pebinha",
+      fixo: "Nogueira",
+      técnico: "Marcelo Batista",
+      vitorias: 4,
+      derrotas:0,
+    },
+  {
+    nome: "Acel chopinzinho futsal",
+    goleiro: "Leo Parisotto",
+    aladireita: "Matheus Mendes",
+    alaesquerda: "Lucas da silva",
+    pivô: "Breno Felipe",
+    fixo: "Cubano",
+    técnico: "Daniel Júnior",
+    vitorias: 1,
+    derrotas:4,
+  } ,
+  {
+    nome: "Itambé/Minas",
+    goleiro: "Anderson",
+    aladireita: "Baron",
+    alaesquerda: "Pintinho",
+    pivô: "Felipão",
+    fixo: "Gui Lopes",
+    técnico: "Diogo Barros",
+    vitorias: 21,
+    derrotas:10,
+  },
+  {
+    nome: "Santo André",
+    goleiro: "Caio",
+    aladireita: "Xaropinho",
+    alaesquerda: "Mendes",
+    pivô: "Dudu",
+    fixo: "Felipe",
+    técnico: "Cidão",
+    vitorias: 20,
+    derrotas:19,
+  },
+  {
+    nome: "Cascavel",
+    goleiro: "Jackson",
+    aladireita: "Elano",
+    alaesquerda: "Zequinha",
+    pivô: "Vini",
+    fixo: "Carlão",
+    técnico: "Deividy",
+    vitorias: 23,
+    derrotas:10,
+  }
+]
+
+
+
+    
